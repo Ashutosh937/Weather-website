e677048659d4491c7a9d40796c433f668a921fa4
unknown <chatterjee.ashutosh937@gmail.com>
2020-07-21 00:22:18 +0530

    Updated forecast

--- utilis/forecast.js
+++ utilis/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=f48c67e7c1ae2ca7256a701174fedae2&query='+latitude+','+longitude+'&units=f'
+  const url = 'http://api.weatherstack.com/current?access_key=f48c67e7c1ae2ca7256a701174fedae2&query='+latitude+','+longitude
   request({ url, json: true}, (error, { body }) => {
     if(error){
       callback('Unable to connect to the weather app.', undefined)
     } else if(body.error) {
       callback('Unable to fetch co-ordinates', undefined)
     } else{
-      callback(undefined,body.current.weather_descriptions[0] + ". It is "+ body.current.temperature+ " degree fahrenheit outside. It feels like " + body.current.feelslike + " degree fahrenheit.")
+      callback(undefined,body.current.weather_descriptions[0] + ". It is "+ body.current.temperature+ " degree celcius outside. It feels like " + body.current.feelslike + " degree celcius. The humidity is "+body.current.humidity+"%.")
     }
   })
 }
